826dd291c2c8ee31a99d5397cab9da5fd36878be
Artem Napolskih <napolskih@gmail.com>
2016-01-01 12:49:21 +0500

    Update react_no_redux_generator.rb

--- lib/generators/react_on_rails/react_no_redux_generator.rb
+++ lib/generators/react_on_rails/react_no_redux_generator.rb
@@ -5,6 +5,10 @@ include GeneratorHelper
 module ReactOnRails
   module Generators
     class ReactNoReduxGenerator < Rails::Generators::Base
+      def self.hide!
+        # noop
+      end  
+      
       hide!
       source_root(File.expand_path("../templates", __FILE__))
 
