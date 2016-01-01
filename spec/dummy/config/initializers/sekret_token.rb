1837527536303165269501ad2affb7607e4d7a04
Artem Napolskih <napolskih@gmail.com>
2016-01-01 23:40:54 +0500

    Create sekret_token.rb

---
+++ spec/dummy/config/initializers/sekret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '0ab357c0aca1441208704b5cac40b76ccad4bfb404cfb3a08fc04ed6dcb7934e909018703d3f744427275a250d82049a1f44a00a720a61284ebeaac801d384ca'
