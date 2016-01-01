9329e568b666a196fdeb331b8fd439aa42fc583b
Artem Napolskih <napolskih@gmail.com>
2016-01-01 12:50:24 +0500

    Update ruby_linters_generator.rb

--- lib/generators/react_on_rails/ruby_linters_generator.rb
+++ lib/generators/react_on_rails/ruby_linters_generator.rb
@@ -3,6 +3,10 @@ require "rails/generators"
 module ReactOnRails
   module Generators
     class RubyLintersGenerator < Rails::Generators::Base
+      def self.hide!
+        # noop
+      end
+            
       hide!
       source_root File.expand_path("../templates", __FILE__)
 
